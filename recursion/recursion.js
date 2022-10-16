a19f78c59961660642875f96fa7aa7fa044c21ed
Beroza Paul <beroza07@gmail.com>
2022-10-16 22:29:46 +0200

    Write a program to calculate the sum of numbers from 1 to n using recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,5 +1,5 @@
 (function(){
-    // Write a program in C to print first 50 natural numbers using recursion.
+    // Write a program to print first 50 natural numbers using recursion.
 
     const printNumber = (num) => {
        if (num < 1) {
@@ -9,7 +9,22 @@
        printNumber(num -1);
        console.log(num);
        return num;
-    }
+    };
 
     printNumber(5)
+})();
+
+(function(){
+    // Write a program to calculate the sum of numbers from 1 to n using recursion.
+
+    const getSum = (arr) => {
+       if (arr.length < 1) {
+          return 0;
+       }
+
+       return arr.pop() + getSum(arr);
+    };
+ 
+    const a = [1, 2, 3, 5];
+    console.log(getSum(a))
 })();
