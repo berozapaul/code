2a521d1b740a70a955705f4d650deb04736ae1ab
Beroza Paul <beroza07@gmail.com>
2022-10-24 06:11:40 +0200

    2169. Count Operations to Obtain Zero

---
+++ 2169/2169.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+ var countOperations = function(num1, num2) {
+    if (num1 === 0 || num2 === 0) {
+        return 0;
+    }
+    
+    if (num1 >= num2) {
+        num1 = num1 - num2;
+    } else {
+        num2 = num2 - num1;
+    }
+    
+    return 1 + countOperations (num1, num2);
+};
