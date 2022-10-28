1d69b1175eb0d2062937f1e3ee7db70c95f8be94
Beroza Paul <beroza07@gmail.com>
2022-10-28 19:56:11 +0200

    283. Move Zeroes

---
+++ 283/283.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var moveZeroes = function(nums) {
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+       if (nums[i] !== 0) {
+           nums[index++] = nums[i];
+       }
+    }
+    nums.fill(0, index);
+};
