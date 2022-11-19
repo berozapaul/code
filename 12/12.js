b62431d467e8c921089511efcd205511f021bed3
Beroza Paul <beroza07@gmail.com>
2022-11-19 09:14:48 +0100

    12. Integer to Roman

---
+++ 12/12.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+ var intToRoman = function(num) {
+    const map = new Map([ 
+        ['M', 1000], 
+        ['CM', 900], 
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+        ]);
+    let out = '';
+    for(const[letter, n] of map.entries()) {
+        if (n > num) continue; 
+        out += letter.repeat(Math.floor(num / n));
+        num %= n;      
+    }
+    return out;    
+};
