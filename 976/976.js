10a1c6b3e4a69be9e89d50c5c9b8e41f38575c13
Beroza Paul <beroza07@gmail.com>
2022-12-01 22:32:03 +0100

    976. Largest Perimeter Triangle

---
+++ 976/976.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var largestPerimeter = function(nums) {
+    nums.sort((a, b) => b - a);
+    const loopCnt = nums.length - 2;
+  
+    for (let i = 0; i < loopCnt; i++) {
+      if (nums[i] < nums[i + 1] + nums[i + 2]) {
+          return nums[i] + nums[i + 1] + nums[i + 2];
+      }
+    }
+    
+    return 0;    
+  };
