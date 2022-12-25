362b564ff5ebf24715592085ae0b8312b3cb7ff3
Beroza Paul <beroza07@gmail.com>
2022-12-25 15:16:15 +0100

    46. Permutations (add another approach)

--- 46/46.js
+++ 46/46.js
@@ -17,3 +17,33 @@
     
     return out;
   };
+
+
+  /**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  const out = []; 
+  const permute = (item, used) => {
+      if (item.length === nums.length) {
+          out.push(Array.from(item));
+          return;
+      }
+      for (let i = 0; i < nums.length; i++) {
+          if (used[i]) {
+              continue; 
+          }
+
+          item.push(nums[i]);
+          used[i] = true;
+          permute(item, used);
+
+          used[i] = false;
+          item.pop();
+      }       
+  };
+
+  permute([], Array(nums.length).fill(false));
+  return out;
+};
