5a4ad9100edf5182298c24888c929a004336d1d9
Beroza Paul <beroza07@gmail.com>
2023-01-17 09:01:43 +0100

    1833. Maximum Ice Cream Bars

---
+++ 1833/1833.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+ var maxIceCream = function(costs, coins) {
+    costs.sort((a, b) => a - b);
+    for (let i = 0; i < costs.length; i++) {  
+        if ((coins -= costs[i]) < 0) {
+            return i;
+        }
+    }  
+    return costs.length;  
+};
