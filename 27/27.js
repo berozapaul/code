cca67119f59272d84c2e83fba446e87fa2a35da6
Beroza Paul <beroza07@gmail.com>
2023-02-05 23:27:45 +0100

    27. Remove Element

---
+++ 27/27.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+    var zeroStartIdx = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==val){
+            nums[zeroStartIdx]=nums[i];
+            zeroStartIdx++
+        }
+    }
+    return zeroStartIdx;     
+};
